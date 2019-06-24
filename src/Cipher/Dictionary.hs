20ebbb2e6e3d2b9d9b8d679091dce8814eda85f7
Brandon Chinn <brandon@leapyear.io>
2019-06-23 23:59:31 -0700

    Add english dictionary

---
+++ src/Cipher/Dictionary.hs
@@ -0,0 +1,22 @@
+module Cipher.Dictionary
+  ( allWordsWithLength
+  ) where
+
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IntMap
+import System.IO.Unsafe (unsafePerformIO)
+
+allWordsWithLength :: Int -> [String]
+allWordsWithLength len = IntMap.findWithDefault [] len englishDictWordLengths
+
+{- Helpers -}
+
+englishDictWordLengths :: IntMap [String]
+englishDictWordLengths = IntMap.fromListWith (++)
+  [ (length word, [word]) | word <- englishDict ]
+
+{-# NOINLINE englishDict #-}
+englishDict :: [String]
+englishDict = unsafePerformIO $
+  -- TODO: fix for linux
+  lines <$> readFile "words.txt"
